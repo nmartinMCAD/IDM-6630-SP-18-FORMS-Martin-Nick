284d37fe05e5bd0f065d169c0e3bf6b0827d6d67
nmartinMCAD <nmartin@mcad.edu>
2018-02-13 11:42:35 -0700

    FORMS Repo - First COMMIT

---
+++ code/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WEEK 05</title>
+  </head>
+  <body>
+    <main>
+      <?php
+
+        $host = '172.25.0.2';
+        $database = 'week05';
+        $user = 'idm6630';
+        $pass = 'idm6630';
+
+        try {
+
+          $conn = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo 'Connection Established';
+
+          $conn = null;
+
+        } catch(PDOException $e) {
+
+            echo 'Connection Failed: ' . $e->getMessage();
+            exit();
+
+        }
+
+
+
+      ?>
+    </main>
+  </body>
+</html>
